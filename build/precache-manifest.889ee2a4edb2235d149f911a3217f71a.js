5ce7f474e0ce599f155164e230064a1f61e812b6
Ivan <iborisov@pryaniky.onmicrosoft.com>
2020-08-04 12:35:48 +0300

    add build

---
+++ build/precache-manifest.889ee2a4edb2235d149f911a3217f71a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbc270a3c7c572e7dafa539c95208cf",
+    "url": "/temp_sushi-yum/index.html"
+  },
+  {
+    "revision": "e8f35a561822c216b891",
+    "url": "/temp_sushi-yum/static/css/main.c09aed55.chunk.css"
+  },
+  {
+    "revision": "ce9c906a7d6bd757a953",
+    "url": "/temp_sushi-yum/static/js/2.770114fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/temp_sushi-yum/static/js/2.770114fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f35a561822c216b891",
+    "url": "/temp_sushi-yum/static/js/main.194e64d3.chunk.js"
+  },
+  {
+    "revision": "98e3c8b6f9fcd13aa869",
+    "url": "/temp_sushi-yum/static/js/runtime-main.427e5ac6.js"
+  },
+  {
+    "revision": "98883b9a64257b5a52301cda0093ed68",
+    "url": "/temp_sushi-yum/static/media/mainHeader.98883b9a.png"
+  }
+]);
